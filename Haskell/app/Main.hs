7513f6c1bf9c5e72cf2d1165966966b24b06799c
NutiNaguti <NutiNaguti@github.com>
2019-07-10 18:55:24 +0400

    bubbl sort

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -25,12 +25,27 @@ factorial n =
         then 1
         else n * factorial(n-1)
 
-bubbleSort :: [Integer] -> [Integer]
-bubbleSort l = sort 0 l
-    where sort n l = 
-        if l == []
-            then n
-            else let x = l   
+------------------------------------------------------------------------------
+-- Однократный прогон перестановок по списку.
+-- Возвращает кортеж (n,ps), где n - число совершенных перестановок, 
+-- а ps - частично отсортированный список
+ 
+transp :: (Ord a) => [a] -> (Int, [a]) 
+transp [x] = (0,[x])
+transp (x1:x2:xs) | (x2 < x1) = (n+1, x2: ts1)  
+                  | otherwise = (m, x1 :  ts2)  
+                    where (n,ts1)=transp (x1:xs)
+                          (m,ts2)=transp (x2:xs)
+                          
+-- Рекурсивное повторение transp до тех пор, 
+-- пока число перестановок не станет = 0 (список отсортирован)
+ 
+bSort :: (Ord a) => [a] -> [a]
+bSort x | (n==0) = sx
+        | otherwise = bSort sx 
+          where (n,sx) = transp x
+
+------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
